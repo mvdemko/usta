af04c44557b9adb98a00a702518b5ed1b2347928
Maegen Demko <mvd326@gmail.com>
2025-07-11 11:14:23 -0700

    First attempt at AI agent-based model for collecting and visualizing USTA tournament data.

---
+++ src/usta/main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime, timedelta
+
+from usta.crew import Usta
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'website': 'https://playtennis.usta.com/tournaments',
+        'today': str(datetime.now().date()),
+        'three_months_from_now': str((datetime.now() + timedelta(days=90)).date()),
+        'home_city': 'San Diego, CA',
+        'category': 'Adult (18+)',
+        'section': 'Southern California',
+        'district': 'Any district',
+        'level': '4.5',
+        'home_city': 'San Diego, CA'
+    }
+    
+    try:
+        Usta().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        'current_year': str(datetime.now().year)
+    }
+    try:
+        Usta().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        Usta().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        "current_year": str(datetime.now().year)
+    }
+    
+    try:
+        Usta().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
