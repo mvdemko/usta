442a94aacbbd1f091c42e430c3e5174c6809385c
Maegen Demko <mvd326@gmail.com>
2025-07-14 08:57:26 -0700

    Adding the stagehand tool for website scraping

--- src/usta/crew.py
+++ src/usta/crew.py
@@ -1,7 +1,12 @@
 from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
+from crewai.project import CrewBase, agent, crew, task, before_kickoff
+from crewai_tools import StagehandTool
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from dotenv import load_dotenv
 from typing import List
+import os
+
+
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
@@ -13,6 +18,10 @@ class Usta():
     agents: List[BaseAgent]
     tasks: List[Task]
 
+    @before_kickoff
+    def get_keys(self, inputs):
+        load_dotenv()
+
     # Learn more about YAML configuration files here:
     # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
     # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
@@ -20,11 +29,17 @@ class Usta():
     # If you would like to add tools to your agents, you can learn more about it here:
     # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
-    def researcher(self) -> Agent:
-        return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
-            verbose=True
-        )
+    def web_scraper(self) -> Agent:
+        with StagehandTool(
+            api_key=os.getenv("BROWSERBASE_API_KEY"),
+            project_id=os.getenv("BROWSERBASE_PROJECT_ID"),
+            model_api_key=os.getenv("ANTHROPIC_API_KEY"),  # OpenAI or Anthropic API key
+        ) as stagehand_tool:
+            return Agent(
+            config=self.agents_config['web_scraper'], # type: ignore[index]
+            verbose=True,
+            tools=[stagehand_tool]
+            )
 
     @agent
     def data_visualization_specialist(self) -> Agent:
@@ -37,9 +52,9 @@ class Usta():
     # task dependencies, and task callbacks, check out the documentation:
     # https://docs.crewai.com/concepts/tasks#overview-of-a-task
     @task
-    def research_task(self) -> Task:
+    def web_scraping_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+            config=self.tasks_config['web_scraping_task'], # type: ignore[index]
             output_file='tournaments.json',
             allow_code_execution=True, # Allows the agent to execute code if needed
         )
